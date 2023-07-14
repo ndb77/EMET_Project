5fd2f9e0e92d63f8586ee20c52a73fd4877bb72a
ndb77 <bastolanoah@gmail.com>
2023-07-14 10:03:16 -0500

    add medication contains NIH api that allows for medication autocomplete and automatic dose strength fill in

--- medication-adherence/src/components/MedicationListTableBodyComponent.js
+++ medication-adherence/src/components/MedicationListTableBodyComponent.js
@@ -23,61 +23,6 @@ const MedicationListTableBodyComponent = ({
                   />
                 );
                 let editValue =
-                  accessor in data["edit"] && data["edit"][accessor].length > 0
-                    ? data["edit"][accessor]
-                    : null;
-                if (data.edit && data.edit.clinicianStopped === true){
-                  editValue = ""
-                }
-                return (
-                  <>
-                    <td key={accessor}>
-                      <p
-                        style={{
-                          textDecoration:
-                            editValue !== null ? "line-through" : null,
-                        }}
-                      >
-                        {tData}
-                      </p>
-                      {medicationListVersion &&
-                      !(
-                        accessor === "confirm" ||
-                        accessor === "change" ||
-                        accessor === "unsure"
-                      ) ? (
-                        <div>
-                          <div className="col">
-                            <p style={{ color: "red" }}>{editValue}</p>
-                          </div>
-                        </div>
-                      ) : null}
-                    </td>
-                  </>
-                );
-              })}
-            </tr>
-          );
-        })}
-      </tbody>
-    );
-  } else if (medicationListVersion === "editedList") {
-    return (
-      <tbody style={{ borderWidth: 1, borderStyle: "solid" }}>
-        {tableData.map((data) => {
-          if (Object.keys(data.edit).length > 0) {
-            return (
-              <tr key={data.id}>
-                {columns.map(({ accessor }) => {
-                  const tData = data[accessor] ? (
-                    data[accessor]
-                  ) : (
-                    <MedicationListTableBodyButton
-                      id={data.id}
-                      selectionType={accessor}
-                    />
-                  );
-                  let editValue =
                     accessor in data["edit"] &&
                     data["edit"][accessor].length > 0
                       ? data["edit"][accessor]
@@ -99,6 +44,82 @@ const MedicationListTableBodyComponent = ({
                       </td>
                     </>
                   );
+              })}
+            </tr>
+          );
+        })}
+      </tbody>
+    );
+  } else if (medicationListVersion === "editedList") {
+    return (
+      <tbody style={{ borderWidth: 1, borderStyle: "solid" }}>
+        {tableData.map((data) => {
+          if (Object.keys(data.edit).length > 0) {
+            return (
+              <tr key={data.id}>
+                {columns.map(({ accessor }) => {
+                  const tData = data[accessor] ? (
+                    data[accessor]
+                  ) : (
+                    <MedicationListTableBodyButton
+                      id={data.id}
+                      selectionType={accessor}
+                    />
+                  );               
+                let editValue =
+                  accessor in data["edit"] && data["edit"][accessor].length > 0
+                    ? data["edit"][accessor]
+                    : null;
+                return (
+                  <>
+                    <td key={accessor}>
+                      <p
+                        style={{
+                          color:editValue?"red":"black",
+                          textDecoration:
+                            editValue !== null ? "line-through" : null,
+                        }}
+                      >
+                          {tData}
+                      </p>
+                      {medicationListVersion &&
+                      !(
+                        accessor === "confirm" ||
+                        accessor === "change" ||
+                        accessor === "unsure"
+                      ) ? (
+                        <div>
+                          <div className="col">
+                            <p style={{ color: "red" }}>{editValue}</p>
+                          </div>
+                        </div>
+                      ) : null}
+                    </td>
+                  </>
+                );
+
+                  // let editValue =
+                  //   accessor in data["edit"] &&
+                  //   data["edit"][accessor].length > 0
+                  //     ? data["edit"][accessor]
+                  //     : null;
+                  // if (data.edit && data.edit.clinicianStopped === true){
+                  //   editValue = ""
+                  // }
+                  // return (
+                  //   <>
+                  //     <td key={accessor}>
+                  //       <p
+                  //         style={{
+                  //           color: editValue !== null ? "red" : "black",
+                  //           textDecoration:editValue===""?"line-through":"none",
+                  //         }}
+                  //       >
+                  //         {editValue ? editValue : tData}
+                  //       </p>
+                  //     </td>
+                  //   </>
+                  // );
                 })}
               </tr>
             );
